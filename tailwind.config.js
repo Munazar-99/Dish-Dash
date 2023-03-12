8cecb33696b6cd152df69040b708eac34eb84395
Munazar-99 <munazar.ali.001@gmail.com>
2023-03-12 11:41:59 +0100

    initialized project and configured tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./Work/React-Native/DishDash/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
